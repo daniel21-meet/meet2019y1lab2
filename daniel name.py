bb8da27197c5192bf03c73c3b84c9b86e72982bf
daniel21-meet <daniel21@meet.mit.edu>
2019-07-22 09:17:02 -0400

    Stopping here

--- daniel name.py
+++ daniel name.py
@@ -1,6 +1,6 @@
 import turtle
 turtle.pendown
-turtle.circle (125, 180)
+turtle.circle (100, 360)
 turtle.left (90)
 turtle.forward (250)
 turtle.mainloop
---
+++ lab_2.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.pendown ()
+turtle.goto (50, 0)
+turtle.goto (25,0)
+turtle.goto (25,100)
+turtle.goto (0,100)
+turtle.penup ()
+turtle.goto (25,70)
+turtle.pendown ()
+turtle.left (90)
+turtle.circle (-50, 180)
+turtle.forward (70)
+turtle.penup
+turtle.goto (150,0)
+turtle.pendown ()
+turtle.goto (100,0)
+
